19293e6d88c27d990fdea8c51699f6a799b360dc
GUIGLI <joel.guigli@gmail.com>
2023-12-22 20:19:27 +0100

    suppression bonus

--- srcs/requirements/bonus/ftp/conf/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-useradd -m -s /bin/bash $FTP_USER
-echo "$FTP_USER" > /etc/vsftpd.userlist
-echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
-
-exec vsftpd
--- srcs/requirements/bonus/redis/conf/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "maxmemory 256mb" >> /etc/redis/redis.conf 
-echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
-echo "bind 127.0.0.1" >> /etc/redis/redis.conf
-
-# exec redis-cli monitor
-exec redis-server --port 6379 --protected-mode no
