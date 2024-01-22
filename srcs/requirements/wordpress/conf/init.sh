e53ec7207fdb5b79d8c7139a47b146b1c655dcbc
GUIGLI <joel.guigli@gmail.com>
2024-01-22 02:30:03 +0100

    ajout port 80 nginx, remplacement du nom de domaine, certif certbot fonctionnel

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -10,7 +10,7 @@ wp core download --path=/var/www/wordpress --allow-root --locale=fr_FR
 # wp config create --path=/var/www/wordpress --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
 
 #     Installe Wordpress en définissant l'URL, le titre, les informations d'administration et en sautant l'envoi d'un email.
-wp core install --path=/var/www/wordpress --allow-root --url=https://jguigli.42.fr --title="CLOUD-1" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=jguigli@42.fr --skip-email
+wp core install --path=/var/www/wordpress --allow-root --url=https://guinuy.duckdns.org --title="CLOUD-1" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=jguigli@42.fr --skip-email
 
 #     Crée un utilisateur Wordpress avec les informations définies pour le nom d'utilisateur, l'email et le mot de passe.
 wp user create --path=/var/www/wordpress --allow-root $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD
@@ -25,7 +25,7 @@ chmod -R 777 /var/www/wordpress/
 # wp config set WP_REDIS_HOST redis --allow-root --path=/var/www/wordpress
 # wp config set WP_REDIS_PORT 6379 --raw --allow-root --path=/var/www/wordpress
 # wp config set WP_REDIS_CLIENT phpredis --allow-root --path=/var/www/wordpress
-# wp config set WP_CACHE_KEY_SALT jguigli.42.fr --allow-root --path=/var/www/wordpress
+# wp config set WP_CACHE_KEY_SALT guinuy.duckdns.org --allow-root --path=/var/www/wordpress
 # wp config set WP_CACHE true --allow-root --path=/var/www/wordpress
 # wp config set FS_METHOD direct --allow-root --path=/var/www/wordpress
 # wp plugin install redis-cache --activate --path=/var/www/wordpress --allow-root
