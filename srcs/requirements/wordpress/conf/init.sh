5b56cde94f61dcf3e3f022f61b172e312b8a01bc
GUIGLI <joel.guigli@gmail.com>
2024-01-22 01:20:46 +0100

    route pma https://domaine/phpmyadmin avec absolute URL OK, mise en place conteneur certbot, pas fonctionnel en local, deploiement sur le cloud

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -5,10 +5,13 @@
 
 #     Télécharge la dernière version de Wordpress dans le répertoire /var/www/wordpress
 wp core download --path=/var/www/wordpress --allow-root --locale=fr_FR
+
 #     Crée un fichier de configuration pour Wordpress avec les informations de la base de données, telles que le nom de la base de données, l'utilisateur, le mot de passe et l'hôte de la base de données MariaDB.
 # wp config create --path=/var/www/wordpress --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+
 #     Installe Wordpress en définissant l'URL, le titre, les informations d'administration et en sautant l'envoi d'un email.
-wp core install --path=/var/www/wordpress --allow-root --url=https://guinuy.duckdns.org --title="Inception" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=jguigli@42.fr --skip-email
+wp core install --path=/var/www/wordpress --allow-root --url=https://jguigli.42.fr --title="CLOUD-1" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=jguigli@42.fr --skip-email
+
 #     Crée un utilisateur Wordpress avec les informations définies pour le nom d'utilisateur, l'email et le mot de passe.
 wp user create --path=/var/www/wordpress --allow-root $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD
 
